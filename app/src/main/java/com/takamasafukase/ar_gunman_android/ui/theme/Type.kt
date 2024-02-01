d12526091bc3c3f5a34e82a367dfe1cc591a4469
Takamasa Fukase <ultrafukase@gmail.com>
2024-02-01 22:53:49 +0900

    Copperplateのカスタムフォントを追加

--- app/src/main/java/com/takamasafukase/ar_gunman_android/ui/theme/Type.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.takamasafukase.ar_gunman_android.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.takamasafukase.ar_gunman_android.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,10 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val copperplate = FontFamily(
+    Font(R.font.copperplate_light, FontWeight.Light),
+    Font(R.font.copperplate, FontWeight.Normal),
+    Font(R.font.copperplate_heavy, FontWeight.Bold),
 )
