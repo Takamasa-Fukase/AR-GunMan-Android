5c8a052432c8e34a3304bb7fa8343f0db5f71f9c
Takamasa Fukase <ultrafukase@gmail.com>
2024-02-10 11:59:05 +0900

    設定画面のプラポリへのURLリンクを追加、遷移できる様にした

--- app/src/main/java/com/takamasafukase/ar_gunman_android/const/UrlConst.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/const/UrlConst.kt
@@ -3,6 +3,6 @@ package com.takamasafukase.ar_gunman_android.const
 class UrlConst {
     companion object {
         const val developerContactUrl = "https://www.instagram.com/takamasa_fukase/"
-        const val privacyPolicyUrl = ""
+        const val privacyPolicyUrl = "https://takamasa-fukase.github.io/AR-GunMan-Android/PrivacyPolicy"
     }
 }
