20cd108f5e2440ebfa6c7c024cec259afceb90bd
Takamasa-Fukase <ultrafukase@gmail.com>
2026-08-14 15:39:56 +0900

    GameFlowEntityを作成

---
+++ domain/src/main/java/com/ar_gunman_android/domain/entities/game/GameFlow.kt
@@ -0,0 +1,32 @@
+package com.ar_gunman_android.domain.entities.game
+
+data class GameFlow(
+    val status: GameFlowStatus = GameFlowStatus.FlowNotStarted
+) {
+    internal fun drive(nextStatus: GameFlowStatus): GameFlow {
+        return this.copy(status = nextStatus)
+    }
+}
+
+sealed interface GameFlowStatus {
+    object FlowNotStarted : GameFlowStatus
+    object CheckingTutorialCompletedStatus : GameFlowStatus
+    object WaitingForTimerStart : GameFlowStatus
+    object TimerStartedAndWaitingForTimerEnd : GameFlowStatus
+    object TimerResumedAndWaitingForTimerEnd : GameFlowStatus
+    object TimerEndedAndWaitingForFlowEnd : GameFlowStatus
+    object FlowEnded : GameFlowStatus
+    data class Blocked(val reason: BlockedReason) : GameFlowStatus
+
+    enum class BlockedReason {
+        TUTORIAL_NOT_COMPLETED,
+        TIMER_PAUSED
+    }
+
+    val isTimerRunning: Boolean
+        get() = when (this) {
+            TimerStartedAndWaitingForTimerEnd,
+            TimerResumedAndWaitingForTimerEnd -> true
+            else -> false
+        }
+}
