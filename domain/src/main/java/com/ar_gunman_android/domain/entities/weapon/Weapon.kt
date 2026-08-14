603a17cb422818833b3e7a37bbe7e7e7e0d103fb
Takamasa-Fukase <ultrafukase@gmail.com>
2026-08-14 20:58:56 +0900

    WeaponEntityの作成（コミット漏れ）

--- domain/src/main/java/com/ar_gunman_android/domain/entities/weapon/Weapon.kt
+++ domain/src/main/java/com/ar_gunman_android/domain/entities/weapon/Weapon.kt
@@ -1,3 +1,50 @@
 package com.ar_gunman_android.domain.entities.weapon
 
-data class Weapon()
+data class Weapon(
+    val currentType: WeaponType = WeaponType.defaultType,
+    val bulletsCount: Int = WeaponType.defaultType.capacity,
+    val isReloading: Boolean = false,
+) {
+    internal fun fire(): Pair<Weapon, WeaponFireResult> {
+        if (isReloading) {
+            return Pair(this, WeaponFireResult.Failure(reason = WeaponFireResult.FailureReason.RELOADING))
+        }
+        if (bulletsCount <= 0) {
+            return Pair(this, WeaponFireResult.Failure(reason = WeaponFireResult.FailureReason.OUT_OF_BULLETS))
+        }
+        return Pair(this.copy(bulletsCount = bulletsCount - 1), WeaponFireResult.Success)
+    }
+
+    internal fun startReload(): Pair<Weapon, WeaponReloadStartResult> {
+        if (isReloading || bulletsCount > 0) {
+            return Pair(this, WeaponReloadStartResult.FAILURE)
+        }
+        return Pair(this.copy(isReloading = true), WeaponReloadStartResult.SUCCESS)
+    }
+
+    internal fun finishReload(): Weapon {
+        return this.copy(
+            bulletsCount = currentType.capacity,
+            isReloading = false,
+        )
+    }
+
+    internal fun change(newType: WeaponType): Weapon {
+        return this.copy(currentType = newType).finishReload()
+    }
+}
+
+sealed interface WeaponFireResult {
+    object Success : WeaponFireResult
+    data class Failure(val reason: FailureReason) : WeaponFireResult
+
+    enum class FailureReason {
+        RELOADING,
+        OUT_OF_BULLETS;
+    }
+}
+
+enum class WeaponReloadStartResult {
+    SUCCESS,
+    FAILURE;
+}
