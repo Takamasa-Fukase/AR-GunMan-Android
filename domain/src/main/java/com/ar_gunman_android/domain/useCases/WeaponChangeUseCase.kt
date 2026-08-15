e3c557d3c0fb74d12c7e58b264f35ef57511df1f
Takamasa-Fukase <ultrafukase@gmail.com>
2026-08-15 14:18:46 +0900

    残りのWeapon操作系のUseCaseも追加

---
+++ domain/src/main/java/com/ar_gunman_android/domain/useCases/WeaponChangeUseCase.kt
@@ -0,0 +1,19 @@
+package com.ar_gunman_android.domain.useCases
+
+import com.ar_gunman_android.domain.entities.weapon.WeaponType
+import com.ar_gunman_android.domain.storeInterfaces.WeaponStoreInterface
+
+interface WeaponChangeUseCaseInterface {
+    fun execute(newType: WeaponType)
+}
+
+class WeaponChangeUseCase(
+    private val weaponStore: WeaponStoreInterface,
+    private val weaponReloadUseCase: WeaponReloadUseCaseInterface,
+) : WeaponChangeUseCaseInterface {
+    override fun execute(newType: WeaponType) {
+        // 既存のリロードをキャンセルする
+        weaponReloadUseCase.stopCurrentReloadIfExists()
+        weaponStore.weapon = weaponStore.weapon.change(newType = newType)
+    }
+}
---
+++ domain/src/main/java/com/ar_gunman_android/domain/useCases/WeaponFireUseCase.kt
@@ -0,0 +1,32 @@
+package com.ar_gunman_android.domain.useCases
+
+import com.ar_gunman_android.domain.entities.weapon.WeaponFireResult
+import com.ar_gunman_android.domain.entities.weapon.WeaponType
+import com.ar_gunman_android.domain.storeInterfaces.WeaponStoreInterface
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+interface WeaponFireUseCaseInterface {
+    val fireResultEvent: SharedFlow<WeaponFireResult>
+    suspend fun execute()
+}
+
+class WeaponFireUseCase(
+    private val weaponStore: WeaponStoreInterface,
+    private val weaponReloadUseCase: WeaponReloadUseCaseInterface,
+) : WeaponFireUseCaseInterface {
+    override val fireResultEvent: SharedFlow<WeaponFireResult> get() = _fireResultEvent.asSharedFlow()
+    private val _fireResultEvent = MutableSharedFlow<WeaponFireResult>()
+
+    override suspend fun execute() {
+        val (updatedWeapon, result) = weaponStore.weapon.fire()
+        weaponStore.weapon = updatedWeapon
+        _fireResultEvent.emit(result)
+
+        if (result == WeaponFireResult.Success && weaponStore.weapon.currentType.reloadType == WeaponType.ReloadType.AUTO) {
+            // リロードを自動的に実行
+            weaponReloadUseCase.execute()
+        }
+    }
+}
