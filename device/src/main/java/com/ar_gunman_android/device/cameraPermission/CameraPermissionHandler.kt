409d99a65b4cd1214d01f32b091c77ea27bcf13c
Takamasa-Fukase <ultrafukase@gmail.com>
2026-08-28 15:09:25 +0900

    CameraPermissionHandlerを実装

--- device/src/main/java/com/ar_gunman_android/device/cameraPermission/CameraPermissionHandler.kt
+++ device/src/main/java/com/ar_gunman_android/device/cameraPermission/CameraPermissionHandler.kt
@@ -1,9 +1,37 @@
 package com.ar_gunman_android.device.cameraPermission
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
 interface CameraPermissionHandlerInterface {
     fun getCameraUsagePermissionGrantedFlag(): Boolean
     fun requestCameraUsagePermission()
 }
 
-class CameraPermissionHandler {
+class CameraPermissionHandler(
+    private val context: Context
+) : CameraPermissionHandlerInterface {
+    private var launcher: ActivityResultLauncher<String>? = null
+
+    fun register(activity: ComponentActivity) {
+        launcher = activity.registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {}
+    }
+
+    override fun getCameraUsagePermissionGrantedFlag(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun requestCameraUsagePermission() {
+        launcher?.launch(Manifest.permission.CAMERA)
+    }
 }
